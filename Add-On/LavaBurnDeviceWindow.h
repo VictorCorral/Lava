07dc46a39c6b9e984d254fc621e698faaaf2f1ca
stargater <ralf.schuelke@googlemail.com>
2013-09-26 11:04:19 +0200

    add BurnDeviceWindow, a Window hoe can select the Device and start Bunrning. This is WIP, not usedable. STATUS: WIP

---
+++ Add-On/LavaBurnDeviceWindow.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2010 stargater <ralf.schuelke@googlemail.com>
+ * All rights reserved. Distributed under the terms of the MIT license.
+ */
+#ifndef LavaBurnDeviceWindow_H
+#define LavaBurnDeviceWindow_H
+
+
+#include <iostream> 
+#include <Alert.h>
+#include <Application.h>
+#include <Button.h>
+#include <List.h>
+#include <ListItem.h>
+#include <ListView.h>
+#include <StringView.h>
+#include <Window.h>
+
+#include "include/BurnDevice.h"
+
+const uint32 CHECKBUTTON = 'TBcb'; 
+const uint32 DEVICELIST = 'TBdl'; 
+
+
+// the window that receives messages
+class LavaBurnDeviceWindow: public BWindow {
+	public:
+		LavaBurnDeviceWindow(BRect r, const char* name, window_type w, uint32 f);
+		virtual void MessageReceived(BMessage *msg);
+		virtual bool QuitRequested();
+		/*{
+			be_app_messenger.SendMessage(B_QUIT_REQUESTED); 
+			return true;
+		}*/
+	
+		~LavaBurnDeviceWindow();
+	
+		BurnDevice *burner;
+		BList *devList;
+		BStringView *information;
+	private:
+	
+};
+
+#endif
